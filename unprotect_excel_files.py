35d80549af0898befd0c7a65917cc04ef8fcadca
olivia.chan <ojyc42@gmail.com>
2020-06-06 20:07:23 -0400

    Fix path creation and remove reference to unused packages. Operation validation removed.

--- unprotect_excel_files.py
+++ unprotect_excel_files.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os, sys
 import pandas as pd
 import win32com.client
 import xlrd
@@ -12,25 +11,22 @@ from pathlib import Path
 protected_data_dir = Path('data_protected/')
 unprotected_data_dir = Path('data_unprotected/')
 
-# Get the paths of files to unprotect
-# https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
-existing_paths = [file
-                  for file in os.listdir(protected_data_dir) 
-                  if os.path.isfile(os.path.join(protected_data_dir,file))]
+## Get the paths of files to unprotect
+# https://stackoverflow.com/questions/39909655/listing-of-all-files-in-directory
+existing_paths = [file for file in protected_data_dir.glob('**/*') if file.is_file()]
 
 # Define function that opens and resave a password protected file
 # Function is used later and has to be defined early.
 # args: 
-def unprotect_xlsx(filename):
+def unprotect_xlsx(file):
     xcl = win32com.client.DispatchEx('Excel.Application')
     
     pw_str = '1' 
 
-    # FIX REQUIRED: need to build file path while specifying the parent directory in protected_data_dir
-    filepath = os.path.abspath(protected_data_dir+filename)
-    writepath = os.path.abspath(unprotected_data_dir) + filename.name # construct new path with old file name
+    # construct new path with old file name
+    writepath = unprotected_data_dir/file.name 
     
-    wb = xcl.Workbooks.Open(filepath,0,False,None,pw_str,pw_str,True)
+    wb = xcl.Workbooks.Open(file.absolute(),0,False,None,pw_str,pw_str,True)
     
     # Unprotect each sheet in the workbook
     # COM object Worksheet uses index which starts at 1
@@ -43,7 +39,7 @@ def unprotect_xlsx(filename):
 
     # Disable displays, save file, close it and quit
     xcl.DisplayAlerts = False
-    wb.SaveAs(writepath)
+    wb.SaveAs(writepath.absolute().__str__()) #function requires string as argument
     wb.Close(True)
     xcl.Quit()
 
@@ -53,7 +49,6 @@ for path in existing_paths:
     print("Try to unprotect it.")       
     try: 
         unprotect_xlsx(path)
-        df = pd.read_excel(path, sheet_name=None) #try validating the result
     except Exception as f:
         print(f)
         print("Unprotecting failed")
